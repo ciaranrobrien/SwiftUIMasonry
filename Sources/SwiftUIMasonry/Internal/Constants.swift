795120f83d76115de483b8c9db8b46930fe08922
Ciaran O'Brien <ciaranrobrien@gmail.com>
2025-10-31 09:08:40 +0000

    Updated for 26 OS releases and Swift Concurrency.

---
+++ Sources/SwiftUIMasonry/Internal/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Ciaran O'Brien on 02/04/2022.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
